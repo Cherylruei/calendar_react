5cae20a7cdfc2e6d63f40ce3a5103e2681c8f097
cheryruei <cherylrueichun@gmail.com>
2023-08-24 18:08:38 +0800

    issue: display month issue

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,29 +3,77 @@ import DateView from './DateView'
 import "./Calendar.scss"
 
 const Calendar = () => {
-    const [currentDate, setCurrentDate] = useState(new Date(2017,5))
-    const currentMonth = currentDate.getMonth()
-    const currentYear = currentDate.getFullYear()
-    // console.log(currentDate)  //(2017,5) => 日期2017/6/1 
-    // console.log(currentMonth) // 5
+    const [currentMonth, setCurrentMonth] = useState(4) // 5月
+    const [currentYear, setCurrentYear] = useState(2017)
+    // const [chosenMonth, setChosenMonth] = useState([true, false, false]) // 被選取的月份位置
+    const [displayMonth, setDisplayMonth] = useState([4, 5, 6]) // 被選取的月份
+    // const [chosenYear, setChosenYear] = useState(2017)
+    // console.log(currentMonth) 0 based 月份 + 1 // 5 
     // console.log(currentYear) // 2017
+  console.log(currentMonth,currentYear)
     const daysOfWeek = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
-  return (
+    
+    const handleShowMonth = (month) => {
+      setCurrentMonth(month)
+    };
+
+    function getDate(year, month, date){
+      return new Date(year, month, date)
+    }
+
+    const setNextMonth = () => {
+      const currentPosition = displayMonth.indexOf(currentMonth)
+      if(currentPosition > 1){
+        const updatedMonths = displayMonth.map(month=> month +1)
+        setDisplayMonth(updatedMonths)
+      } else {
+        setCurrentMonth(currentMonth + 1)
+      }
+    }
+    
+    const setPreviousMonth = () =>{
+      const currentPosition = displayMonth.indexOf(currentMonth)
+      if(currentPosition === 0){
+        const updatedMonths = displayMonth.map(month=> month - 1)
+        setDisplayMonth(updatedMonths)
+      } else {
+        setCurrentMonth(currentMonth - 1)
+      }
+    }
+   
+   function updatedYear(month){
+     const newYear = currentYear + Math.floor((month)/12)
+     const newMonth = month % 12 +1
+    //  setCurrentMonth(newMonth)
+    //  setCurrentYear(newYear)
+  console.log("2222",newYear,newMonth)
+     return <div className={`shownMonth ${currentMonth === month ? 'selected' : ''}`} key={month} onClick={() => handleShowMonth(month)}>
+                <p>{currentYear + Math.floor((month)/12)}</p>
+                <p>{month % 12 +1} 月</p>
+              </div>
+            
+          
+   }
+
+    return (
     <div className='calendar'>
        <div className='monthPicker'>
-        <div className='previous'></div>
-        <div className='displayMonth'>
-          <div className='month'><p>{currentYear}</p><p>{currentMonth}月</p></div>
-          <div className='month'><p>{currentYear}</p><p>{Number(currentMonth) +1}月</p></div>
-          <div className='month'><p>{currentYear}</p><p>{Number(currentMonth) +2}月</p></div>
+        <div className='previous' onClick={setPreviousMonth}></div>
+        <div className='months'>
+          {displayMonth.map((month)=> 
+          // console.log("333")
+            updatedYear(month)
+          )}
         </div>
-        <div className='next'></div>
+        <div className='next' onClick={setNextMonth}></div>
        </div>
        <div>
         <div className='displayWeek'>
-        {daysOfWeek.map((item)=><div className="week">{item}</div>)}
+        {daysOfWeek.map((item,index)=><div className="week" key={index}>{item}</div>)}
         </div>
-        <DateView currentDate={currentDate}/>
+        <DateView 
+        currentMonth={currentMonth} 
+        currentYear={currentYear}/>
        </div>
     </div>
   )
--- src/shared/date.js
+++ src/shared/date.js
@@ -1,25 +1,31 @@
 
+
+export function getDate(year, month, date){
+  return new Date(year, month, date)
+}
+
 export function getBeginOfMonth(year, month){
-   // (2023, 8) 會得到 9/1 在星期的哪一天,如果要算 8/1，month 就要輸入 7
-   return new Date(year, month -1).getDay()
+   return new Date(year, month).getDay()
    // Sunday - Saturday : 0 - 6
 }
 
+
+
 export function getfirstOfNextMonth(year, month){
-  return (new Date(year, month).getDay()) 
+  return (new Date(year, month + 1).getDay()) 
   // 會返回 next month 的星期幾 0-6   
   // -1 是拿到前一天的星期幾 (週六會變成 -1) 或是不要相減
 }
  
 
 export function getDaysInMonth(year, month){
-  return new Date(year, month, 0).getDate()
+  return new Date(year, month +1, 0).getDate()
 }
 
 
-console.log(getDaysInMonth(2023, 8))  //31
-console.log(getBeginOfMonth(2023, 8)) 
-console.log(getfirstOfNextMonth(2023, 9))
+// console.log(getDaysInMonth(2023, 8))  //9月
+// console.log(getBeginOfMonth(2023, 8)) // get 9/1
+// console.log(getfirstOfNextMonth(2023, 9))
 // getDay() 可以從特定日期得到星期幾
 // const birthday = new Date('August 19, 2023 23:15:30');
 // const day1 = birthday.getDay();
