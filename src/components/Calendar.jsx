1e291341108b466ca19ccfa44158badf7b5bd06e
cheryruei <cherylrueichun@gmail.com>
2023-08-25 11:25:55 +0800

    fix: calendar display issue

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,82 +1,106 @@
-import {useState} from 'react'
-import DateView from './DateView'
-import "./Calendar.scss"
+import { useRef, useState } from "react";
+import DateView from "./DateView";
+import "./Calendar.scss";
 
 const Calendar = () => {
-    const [currentMonth, setCurrentMonth] = useState(4) // 5月
-    const [currentYear, setCurrentYear] = useState(2017)
-    // const [chosenMonth, setChosenMonth] = useState([true, false, false]) // 被選取的月份位置
-    const [displayMonth, setDisplayMonth] = useState([4, 5, 6]) // 被選取的月份
-    // const [chosenYear, setChosenYear] = useState(2017)
-    // console.log(currentMonth) 0 based 月份 + 1 // 5 
-    // console.log(currentYear) // 2017
-  console.log(currentMonth,currentYear)
-    const daysOfWeek = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
-    
-    const handleShowMonth = (month) => {
-      setCurrentMonth(month)
-    };
+  const [currentMonth, setCurrentMonth] = useState(9); // 5月
+  const [currentYear, setCurrentYear] = useState(2017);
+  const [displayMonth, setDisplayMonth] = useState([
+    { year: 2017, month: 9 },
+    { year: 2017, month: 10 },
+    { year: 2017, month: 11 },
+  ]);
+  const newDisplayMonthsRef = useRef([]);
 
-    function getDate(year, month, date){
-      return new Date(year, month, date)
-    }
+  const daysOfWeek = [
+    "星期日",
+    "星期一",
+    "星期二",
+    "星期三",
+    "星期四",
+    "星期五",
+    "星期六",
+  ];
 
-    const setNextMonth = () => {
-      const currentPosition = displayMonth.indexOf(currentMonth)
-      if(currentPosition > 1){
-        const updatedMonths = displayMonth.map(month=> month +1)
-        setDisplayMonth(updatedMonths)
-      } else {
-        setCurrentMonth(currentMonth + 1)
-      }
-    }
-    
-    const setPreviousMonth = () =>{
-      const currentPosition = displayMonth.indexOf(currentMonth)
-      if(currentPosition === 0){
-        const updatedMonths = displayMonth.map(month=> month - 1)
-        setDisplayMonth(updatedMonths)
-      } else {
-        setCurrentMonth(currentMonth - 1)
-      }
-    }
-   
-   function updatedYear(month){
-     const newYear = currentYear + Math.floor((month)/12)
-     const newMonth = month % 12 +1
-    //  setCurrentMonth(newMonth)
-    //  setCurrentYear(newYear)
-  console.log("2222",newYear,newMonth)
-     return <div className={`shownMonth ${currentMonth === month ? 'selected' : ''}`} key={month} onClick={() => handleShowMonth(month)}>
-                <p>{currentYear + Math.floor((month)/12)}</p>
-                <p>{month % 12 +1} 月</p>
-              </div>
-            
-          
-   }
+  const handleShowMonth = (month) => {
+    setCurrentMonth(month);
+  };
 
-    return (
-    <div className='calendar'>
-       <div className='monthPicker'>
-        <div className='previous' onClick={setPreviousMonth}></div>
-        <div className='months'>
-          {displayMonth.map((month)=> 
-          // console.log("333")
-            updatedYear(month)
-          )}
+  const setNextMonth = () => {
+    const currentPosition = displayMonth.findIndex(
+      (item) => item.month === currentMonth
+    );
+
+    if (currentPosition > 1) {
+      const updatedMonths = displayMonth.map((item) => ({
+        year: item.year + Math.floor((item.month + 1) / 12),
+        month: (item.month + 1) % 12,
+      }));
+
+      setDisplayMonth(updatedMonths);
+      setCurrentMonth(updatedMonths[2].month);
+      setCurrentYear(updatedMonths[2].year);
+    } else {
+      setCurrentMonth(currentMonth + 1);
+    }
+  };
+
+  const setPreviousMonth = () => {
+    const currentPosition = displayMonth.indexOf(currentMonth);
+    if (currentPosition === 0) {
+      const updatedMonths = displayMonth.map((month) => month - 1);
+      setDisplayMonth(updatedMonths);
+    } else {
+      setCurrentMonth(currentMonth - 1);
+    }
+  };
+
+  function UpdatedYear() {
+    const newDisplayMonths = [];
+    return displayMonth.map((item) => {
+      newDisplayMonths.push({
+        year: item.year + Math.floor(item.month / 12),
+        month: (item.month + 1) % 12,
+      });
+      newDisplayMonthsRef.current = newDisplayMonths;
+
+      return (
+        <div
+          className={`shownMonth ${
+            currentMonth === item.month ? "selected" : ""
+          }`}
+          key={item.month}
+          onClick={() => handleShowMonth(item.month)}
+        >
+          <p>{item.year}</p>
+          {/* 讓 item.month 的數字只存在於 0-11 */}
+          <p>{(item.month + 1) % 12 === 0 ? 12 : (item.month + 1) % 12}月</p>
         </div>
-        <div className='next' onClick={setNextMonth}></div>
-       </div>
-       <div>
-        <div className='displayWeek'>
-        {daysOfWeek.map((item,index)=><div className="week" key={index}>{item}</div>)}
+      );
+    });
+  }
+
+  return (
+    <div className="calendar">
+      <div className="monthPicker">
+        <div className="previous" onClick={setPreviousMonth}></div>
+        <div className="months">
+          <UpdatedYear />
         </div>
-        <DateView 
-        currentMonth={currentMonth} 
-        currentYear={currentYear}/>
-       </div>
+        <div className="next" onClick={setNextMonth}></div>
+      </div>
+      <div>
+        <div className="displayWeek">
+          {daysOfWeek.map((item, index) => (
+            <div className="week" key={index}>
+              {item}
+            </div>
+          ))}
+        </div>
+        <DateView currentMonth={currentMonth} currentYear={currentYear} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Calendar
+export default Calendar;
--- src/components/DateView.jsx
+++ src/components/DateView.jsx
@@ -1,27 +1,37 @@
-import React from 'react'
-import { getBeginOfMonth, getDaysInMonth, getfirstOfNextMonth } from '../shared/date'
+import React from "react";
+import {
+  getBeginOfMonth,
+  getDaysInMonth,
+  getfirstOfNextMonth,
+} from "../shared/date";
 
-const DateView = ({currentMonth, currentYear}) => {
-   // currentMonth 11 + 1 = 12 (2017.11) => get 12月
-  const startDayOfWeek = getBeginOfMonth(currentYear, currentMonth)
-  const daysOfMonth = getDaysInMonth(currentYear, currentMonth)
-  const leftDays = (7 - Number(getfirstOfNextMonth(currentYear, currentMonth) ))%7
-  // console.log(currentDate)
-  console.log(currentYear)
+const DateView = ({ currentMonth, currentYear }) => {
+  // currentMonth 11 + 1 = 12 (2017.11) => get 12月
+  const startDayOfWeek = getBeginOfMonth(currentYear, currentMonth);
+  const daysOfMonth = getDaysInMonth(currentYear, currentMonth);
+  const leftDays =
+    (7 - Number(getfirstOfNextMonth(currentYear, currentMonth))) % 7;
+
   return (
-      <div className='displayDate'>
-             {/* 填補前面的空格 */}
-            {Array.from({length:startDayOfWeek}).map((_, index) => {
-            return(<div key={index} className="empty"></div>)})}
-            {Array.from({length:daysOfMonth}).map((_, index) => {
-            const date = index + 1
-            return(<div key={date} className="date">{date}</div>)})}
-            {/* 填補後面的空格 */}
-            {Array.from({length:leftDays}).map((_, index) => {
-            return(<div key={index} className="empty"></div>)})}
-        </div>
- 
-  )
-}
+    <div className="displayDate">
+      {/* 填補前面的空格 */}
+      {Array.from({ length: startDayOfWeek }).map((_, index) => {
+        return <div key={index} className="empty"></div>;
+      })}
+      {Array.from({ length: daysOfMonth }).map((_, index) => {
+        const date = index + 1;
+        return (
+          <div key={date} className="date">
+            {date}
+          </div>
+        );
+      })}
+      {/* 填補後面的空格 */}
+      {Array.from({ length: leftDays }).map((_, index) => {
+        return <div key={index} className="empty"></div>;
+      })}
+    </div>
+  );
+};
 
-export default DateView
+export default DateView;
