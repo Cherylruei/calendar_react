c549c91bd067ade6567b68fbe82425ea42652ff4
cherylruei <cherylrueichun@gmail.com>
2023-08-27 23:45:07 +0800

    fix: wrong time issue

--- src/components/DateView.jsx
+++ src/components/DateView.jsx
@@ -1,27 +1,29 @@
-import { useState } from "react";
+import { useState } from 'react';
 import {
   getBeginOfMonth,
   getDaysInMonth,
   getfirstOfNextMonth,
-} from "../shared/date";
-import { processedData } from "../data/data";
-import "./DateView.scss";
-
+} from '../shared/date';
+import { processedData } from '../data/data';
+import './DateView.scss';
+// console.log(processedData);
 const DateView = ({ currentMonth, currentYear }) => {
   // currentMonth 11 + 1 = 12 (2017.11) => get 12月
   const [chosenDay, setChosenDay] = useState();
   const startDayOfWeek = getBeginOfMonth(currentYear, currentMonth);
-  const daysOfMonth = getDaysInMonth(currentYear, currentMonth);
+  const numberOfDaysInMonth = getDaysInMonth(currentYear, currentMonth);
+
   const leftDays =
     (7 - Number(getfirstOfNextMonth(currentYear, currentMonth))) % 7;
 
+  //  2017/10/1 的資料填入到 2017/09/30
   function CheckTheDate() {
-    return Array.from({ length: daysOfMonth }).map((_, index) => {
+    return Array.from({ length: numberOfDaysInMonth }).map((_, index) => {
       const date = index + 1;
       // 待重構將Fn 放在 date.js 處理，引用該 fn
       const day = new Date(currentYear, currentMonth, date);
-      const month = String(currentMonth + 1).padStart(2, "0");
-      const daysOfMonth = String(day.getDate()).padStart(2, "0");
+      const month = String(currentMonth + 1).padStart(2, '0');
+      const daysOfMonth = String(day.getDate()).padStart(2, '0');
       const composedDate = `${String(currentYear)}-${month}-${daysOfMonth}`;
 
       let matchingEvents;
@@ -29,59 +31,59 @@ const DateView = ({ currentMonth, currentYear }) => {
         matchingEvents = [];
         matchingEvents.push(processedData[composedDate]);
       }
-      matchingEvents = matchingEvents ? matchingEvents[0] : "";
+      matchingEvents = matchingEvents ? matchingEvents[0] : '';
       const newFormatPrice = String(matchingEvents.price).replace(
         /\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g,
-        ","
+        ','
       );
       const statusOptions = {
-        reserved: "預定",
-        unAvailable: "額滿",
-        closed: "截止",
-        openForSignup: "報名",
-        candidacy: "後補",
+        reserved: '預定',
+        unAvailable: '額滿',
+        closed: '截止',
+        openForSignup: '報名',
+        candidacy: '後補',
 
         // contact: "請洽專員",
       };
       let statusStyle;
       switch (matchingEvents?.status) {
         case statusOptions.reserved:
-          statusStyle = "orange";
+          statusStyle = 'orange';
           break;
         case statusOptions.unAvailable:
-          statusStyle = "lightgrey";
+          statusStyle = 'lightgrey';
           break;
         case statusOptions.closed:
-          statusStyle = "lightgrey";
+          statusStyle = 'lightgrey';
           break;
         case statusOptions.openForSignup:
-          statusStyle = "orange";
+          statusStyle = 'orange';
           break;
         case statusOptions.candidacy:
-          statusStyle = "teal";
+          statusStyle = 'teal';
           break;
         default:
-          statusStyle = "blue";
+          statusStyle = 'blue';
           break;
       }
 
       return (
-        <div key={date} className="date">
-          <div className="day">
+        <div key={date} className='date'>
+          <div className='day'>
             <div>{date}</div>
-            {matchingEvents?.guaranteed && <div className="group">成團</div>}
+            {matchingEvents?.guaranteed && <div className='group'>成團</div>}
           </div>
 
           {matchingEvents && (
-            <div className="event">
+            <div className='event'>
               <p className={`status ${statusStyle} `}>
                 {matchingEvents.status}
               </p>
-              <p className="availableVacancy">
+              <p className='availableVacancy'>
                 可賣:{matchingEvents.available}
               </p>
-              <p className="totalVacancy">席次:{matchingEvents.total}</p>
-              <p className="price">${newFormatPrice}</p>
+              <p className='totalVacancy'>席次:{matchingEvents.total}</p>
+              <p className='price'>${newFormatPrice}</p>
             </div>
           )}
         </div>
@@ -90,15 +92,15 @@ const DateView = ({ currentMonth, currentYear }) => {
   }
 
   return (
-    <div className="displayDate">
+    <div className='displayDate'>
       {/* 填補前面的空格 */}
       {Array.from({ length: startDayOfWeek }).map((_, index) => {
-        return <div key={index} className="empty"></div>;
+        return <div key={index} className='empty'></div>;
       })}
       <CheckTheDate />
       {/* 填補後面的空格 */}
       {Array.from({ length: leftDays }).map((_, index) => {
-        return <div key={index} className="empty"></div>;
+        return <div key={index} className='empty'></div>;
       })}
     </div>
   );
--- src/data/data.js
+++ src/data/data.js
@@ -3,29 +3,52 @@ import data2 from './data2.json';
 import data3 from './data3.json';
 import data4 from './data4.json';
 
-//  以 data1 的屬性名稱來做資料整理
-const processData2 = data2.map((item) => (
-{
-    guaranteed: item.certain,
-    date: item.date,
-    price: item.price,
-    available: item.onsell,
-    total: item.total,
-    status: item.state
-})
-)
+//  以 jquery 列的屬性名稱來做資料整理
+const processData1 = data1.map((item) => ({
+  ...item,
+  available: item.availableVancancy,
+  total: item.totalVacnacy,
+}));
+
+const processData2 = data2.map((item) => ({
+  guaranteed: item.certain,
+  date: item.date,
+  price: item.price,
+  available: item.onsell,
+  total: item.total,
+  status: item.state,
+}));
+
+const processData3 = data3.map((item) => ({
+  ...item,
+  available: item.availableVancancy,
+  total: item.totalVacnacy,
+}));
+const processData4 = data4.map((item) => ({
+  ...item,
+  available: item.availableVancancy,
+  total: item.totalVacnacy,
+}));
 
 // 建立以日期為鍵的物件
-export const processedData = [  data1,
-    processData2,
-    data3,
-    data4].reduce((result, group) => {
-        group.forEach((item) => {
-          const itemDate = new Date(item.date);
-          const dateString = itemDate.toISOString().split("T")[0]; // 轉換成 yyyy-mm-dd 格式
-          result[dateString] = item;
-        });
-        return result;
-      }, {});
-   
+export const processedData = [
+  processData1,
+  processData2,
+  processData3,
+  processData4,
+].reduce((result, group) => {
+  group.forEach((item) => {
+    const itemDate = new Date(item.date);
+    const localDate = new Date(
+      itemDate.getTime() - itemDate.getTimezoneOffset() * 60000
+    ); // 調整時區偏移量
+    const dateString = localDate.toISOString().split('T')[0];
 
+    // console.log('item', item);
+    // console.log('localDate');
+    // console.log('dateString', dateString);
+    // 轉換成 yyyy-mm-dd 格式
+    result[dateString] = item;
+  });
+  return result;
+}, {});
