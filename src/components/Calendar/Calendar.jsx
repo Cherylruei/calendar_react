59032395af142baa2dfe6d273b8c9bec631f7324
cheryruei <cherylrueichun@gmail.com>
2023-09-05 15:07:39 +0800

    refactor: filter monthly data before pass it down

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -7,11 +7,6 @@ import { MonthsDisplay } from "../MonthDispay/MonthDisplay";
 const Calendar = () => {
   const [currentMonth, setCurrentMonth] = useState(9);
   const [currentYear, setCurrentYear] = useState(2017);
-  const [displayMonth, setDisplayMonth] = useState([
-    { year: 2017, month: 9 },
-    { year: 2017, month: 10 },
-    { year: 2017, month: 11 },
-  ]);
   const [chosenDay, setChosenDay] = useState(null);
 
   const daysOfWeek = [
@@ -24,66 +19,29 @@ const Calendar = () => {
     "星期六",
   ];
 
-  const setNextMonth = () => {
-    const currentPosition = displayMonth.findIndex(
-      (item) => item.month === currentMonth
+  function getDataPerMonth(currentYear, currentMonth) {
+    // 將陣列轉回物件以 array[0] 為 key
+    return Object.fromEntries(
+      // 提取 processedData 的 key 值來做比對, 將物件轉成陣列做篩選
+      Object.entries(processedData).filter(([key]) => {
+        const [yearStr, monthStr] = key.split("/");
+        return (
+          yearStr === currentYear.toString() &&
+          monthStr === (currentMonth + 1).toString().padStart(2, "0")
+        );
+      })
     );
-
-    if (currentPosition > 1) {
-      const updatedMonths = displayMonth.map((item) => ({
-        year: item.year + Math.floor((item.month + 1) / 12),
-        month: (item.month + 1) % 12,
-      }));
-
-      const lastIndex = updatedMonths.length - 1;
-      setDisplayMonth(updatedMonths);
-      setCurrentMonth(updatedMonths[lastIndex].month);
-      setCurrentYear(updatedMonths[lastIndex].year);
-    } else {
-      setCurrentMonth(currentMonth + 1);
-    }
-    setChosenDay(null);
-  };
-
-  const setPreviousMonth = () => {
-    const currentPosition = displayMonth.findIndex(
-      (item) => item.month === currentMonth
-    );
-    const updatedMonths = displayMonth.map((item) => ({
-      year: item.year + Math.floor((item.month - 1) / 12),
-      month: (item.month - 1) % 12 === -1 ? 11 : (item.month - 1) % 12,
-    }));
-    if (currentPosition === 0) {
-      setDisplayMonth(updatedMonths);
-      setCurrentMonth(updatedMonths[0].month);
-      setCurrentYear(updatedMonths[0].year);
-    } else {
-      setCurrentMonth(currentMonth - 1);
-    }
-    setChosenDay(null);
-  };
+  }
 
   return (
     <div className="calendar">
-      <div className="monthPicker">
-        <div onClick={setPreviousMonth} className="buttonWrap">
-          <div className="previous"></div>
-        </div>
-
-        <div className="months">
-          <MonthsDisplay
-            displayMonth={displayMonth}
-            data={processedData}
-            currentMonth={currentMonth}
-            setCurrentMonth={setCurrentMonth}
-            setCurrentYear={setCurrentYear}
-            setChosenDay={setChosenDay}
-          />
-        </div>
-        <div onClick={setNextMonth} className="buttonWrap">
-          <div className="next"></div>
-        </div>
-      </div>
+      <MonthsDisplay
+        data={processedData}
+        currentMonth={currentMonth}
+        setCurrentMonth={setCurrentMonth}
+        setCurrentYear={setCurrentYear}
+        setChosenDay={setChosenDay}
+      />
       <div>
         <div className="displayWeek">
           {daysOfWeek.map((item, index) => (
@@ -95,7 +53,7 @@ const Calendar = () => {
         <DateView
           currentMonth={currentMonth}
           currentYear={currentYear}
-          data={processedData}
+          data={getDataPerMonth(currentYear, currentMonth)}
           chosenDay={chosenDay}
           setChosenDay={setChosenDay}
         />
--- src/components/MonthDispay/MonthDisplay.jsx
+++ src/components/MonthDispay/MonthDisplay.jsx
@@ -1,36 +1,96 @@
+import { useState } from "react";
+
 export function MonthsDisplay({
-  displayMonth,
   data,
   currentMonth,
   setCurrentMonth,
   setCurrentYear,
   setChosenDay,
 }) {
+  const [displayMonth, setDisplayMonth] = useState([
+    { year: 2017, month: 9 },
+    { year: 2017, month: 10 },
+    { year: 2017, month: 11 },
+  ]);
+
   const handleShowMonth = (year, month) => {
     setCurrentMonth(month);
     setCurrentYear(year);
     setChosenDay(null);
   };
 
-  return displayMonth.map((item) => {
-    const hasGroup = hasDataForMonth(data, item.year, item.month);
-    return (
-      <div
-        className={`shownMonth ${
-          currentMonth === item.month ? "selected" : ""
-        }`}
-        key={item.month}
-        onClick={() => handleShowMonth(item.year, item.month)}
-      >
-        <div className="yearAndMonth">
-          <p>{item.year}</p>
-          {/* 讓 item.month 的數字只存在於 0-11 */}
-          <p>{(item.month + 1) % 12 === 0 ? 12 : (item.month + 1) % 12}月</p>
-        </div>
-        {!hasGroup && <div className="noGroup">無出發日</div>}
-      </div>
+  const setNextMonth = () => {
+    const currentPosition = displayMonth.findIndex(
+      (item) => item.month === currentMonth
     );
-  });
+
+    if (currentPosition > 1) {
+      const updatedMonths = displayMonth.map((item) => ({
+        year: item.year + Math.floor((item.month + 1) / 12),
+        month: (item.month + 1) % 12,
+      }));
+
+      const lastIndex = updatedMonths.length - 1;
+      setDisplayMonth(updatedMonths);
+      setCurrentMonth(updatedMonths[lastIndex].month);
+      setCurrentYear(updatedMonths[lastIndex].year);
+    } else {
+      setCurrentMonth(currentMonth + 1);
+    }
+    setChosenDay(null);
+  };
+
+  const setPreviousMonth = () => {
+    const currentPosition = displayMonth.findIndex(
+      (item) => item.month === currentMonth
+    );
+    const updatedMonths = displayMonth.map((item) => ({
+      year: item.year + Math.floor((item.month - 1) / 12),
+      month: (item.month - 1) % 12 === -1 ? 11 : (item.month - 1) % 12,
+    }));
+    if (currentPosition === 0) {
+      setDisplayMonth(updatedMonths);
+      setCurrentMonth(updatedMonths[0].month);
+      setCurrentYear(updatedMonths[0].year);
+    } else {
+      setCurrentMonth(currentMonth - 1);
+    }
+    setChosenDay(null);
+  };
+
+  return (
+    <div className="monthPicker">
+      <div onClick={setPreviousMonth} className="buttonWrap">
+        <div className="previous"></div>
+      </div>
+      <div className="months">
+        {displayMonth.map((item) => {
+          const hasGroup = hasDataForMonth(data, item.year, item.month);
+          return (
+            <div
+              className={`shownMonth ${
+                currentMonth === item.month ? "selected" : ""
+              }`}
+              key={item.month}
+              onClick={() => handleShowMonth(item.year, item.month)}
+            >
+              <div className="yearAndMonth">
+                <p>{item.year}</p>
+                {/* 讓 item.month 的數字只存在於 0-11 */}
+                <p>
+                  {(item.month + 1) % 12 === 0 ? 12 : (item.month + 1) % 12}月
+                </p>
+              </div>
+              {!hasGroup && <div className="noGroup">無出發日</div>}
+            </div>
+          );
+        })}
+      </div>
+      <div onClick={setNextMonth} className="buttonWrap">
+        <div className="next"></div>
+      </div>
+    </div>
+  );
 }
 
 function hasDataForMonth(dataObject, year, month) {
